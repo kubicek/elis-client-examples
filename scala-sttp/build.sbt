fd21f1a9b160bef2408908bad8bc2afb092ea680
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2018-07-11 15:50:03 +0200

    Add CLI arguments via scopt for the Scala example.
    Also split the example into a class (elis client) and a singleton object (app).

--- scala-sttp/build.sbt
+++ scala-sttp/build.sbt
@@ -8,5 +8,6 @@ libraryDependencies ++= Seq(
   // https://softwaremill.com/introducing-sttp-the-scala-http-client/
   "com.softwaremill.sttp" %% "core" % "1.0.5",
   "com.softwaremill.sttp" %% "json4s" % "1.2.2",
-  "org.json4s" %% "json4s-jackson" % "3.5.4"
+  "org.json4s" %% "json4s-jackson" % "3.5.4",
+  "com.github.scopt" %% "scopt" % "3.7.0"
 )
--- scala-sttp/src/main/scala/ai/rossum/elis/example/ElisClientExampleSttp.scala
+++ scala-sttp/src/main/scala/ai/rossum/elis/example/ElisClientExampleSttp.scala
@@ -6,22 +6,45 @@ import com.softwaremill.sttp._
 import com.softwaremill.sttp.json4s._
 import org.json4s.native.Serialization.writePretty
 
-object ElisClientExampleSttp {
+object ElisClientExampleSttpApp {
+
+  def main(args: Array[String]): Unit = {
+    val parser = new scopt.OptionParser[Config]("elis-example") {
+      head("elis-example", "1.0")
+      opt[String]("filePath").required().action((x, c) =>
+        c.copy(filePath = x)).text("path to document (PDF/PNG)")
+      opt[String]("secretKey").required().action((x, c) =>
+        c.copy(secretKey = x)).text("secret API key")
+      opt[String]("host").action((x, c) =>
+        c.copy(host = x)).text("API host base URL")
+
+      override def showUsageOnError = true
+    }
+
+    parser.parse(args, Config()) match {
+      case Some(config) =>
+        val client = new ElisClientExampleSttp(config.secretKey, config.host)
+        client.sendDocumentAndWaitForResult(config.filePath)
+      case None =>
+    }
+  }
+}
+
+case class Config(filePath: String = "", secretKey: String = "", host: String = "https://all.rir.rossum.ai")
+
+class ElisClientExampleSttp(secretKey: String, host: String) {
+
   //noinspection TypeAnnotation
   implicit val backend = HttpURLConnectionBackend()
 
   //noinspection TypeAnnotation
   implicit val formats = org.json4s.DefaultFormats
 
-  val api_key = "xxxxxxxxxxxxxxxxxxxxxx_YOUR_ELIS_API_KEY_xxxxxxxxxxxxxxxxxxxxxxx"
-  val endpoint = "https://all.rir.rossum.ai"
-
-  def main(args: Array[String]): Unit = {
-     val filePath = "../data/invoice.pdf"
-//    val filePath = "../data/invoice.png"
+  def sendDocumentAndWaitForResult(filePath: String): Unit = {
     sendDocument(filePath).flatMap(getDocumentPolling(_)) match {
       case Right(invoice) =>
         println(s"Processed invoice:\n${writePretty(invoice)}")
+        // --> here we can process the extracted invoice in JSON format <--
       case Left(error) =>
         println(s"Error while processing invoice:\n$error")
     }
@@ -33,8 +56,8 @@ object ElisClientExampleSttp {
     val contentType = documentType(filePath)
 
     val response = sttp
-      .post(uri"$endpoint/document")
-      .header("Authorization", s"secret_key $api_key")
+      .post(uri"$host/document")
+      .header("Authorization", s"secret_key $secretKey")
       .multipartBody(multipartFile("file", file).contentType(contentType))
       .response(asJson[Map[String, Any]])
       .send()
@@ -50,8 +73,8 @@ object ElisClientExampleSttp {
 
   def getDocument(docId: String): Either[String, Map[String, Any]] = {
     val response = sttp
-      .get(uri"$endpoint/document/$docId")
-      .header("Authorization", s"secret_key $api_key")
+      .get(uri"$host/document/$docId")
+      .header("Authorization", s"secret_key $secretKey")
       .response(asJson[Map[String, Any]])
       .send()
     response.body.flatMap(r =>
